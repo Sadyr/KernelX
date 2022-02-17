63043145c5e8218f558cdc11c4a1b72f42d7de06
Sauytov Sadyr <sadyr0012@mail.com>
2022-02-17 22:02:53 +0600

    update cs.txt

--- Kernelx/network/chatroom/server.py
+++ Kernelx/network/chatroom/server.py
@@ -1,39 +1,40 @@
 import socket
 from threading import Thread
 
+
 # server's IP address
 SERVER_HOST = "0.0.0.0"
 SERVER_PORT = 5002 # port we want to use
-separator_token = "<SEP>" # we will use this to separate the client name & message
+separator_token = "<SEP>" # we will use this to separate the clietn name & message
 
 # initialize list/set of all connected client's sockets
 client_sockets = set()
 # create a TCP socket
 s = socket.socket()
-# make the port as reusable port
-s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-# bind the socket to the address we specified
+# make hte port as reusable port
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,1)
 s.bind((SERVER_HOST, SERVER_PORT))
-# listen for upcoming connections
+# listen for upcoming connection
 s.listen(5)
-print(f"[*] Listening as {SERVER_HOST}:{SERVER_PORT}")
+print(f"[*] lisstening as {SERVER_HOST}:{SERVER_PORT}")
+
 
 def listen_for_client(cs):
     """
-    This function keep listening for a message from `cs` socket
-    Whenever a message is received, broadcast it to all other connected clients
+    THis function keep listening for a message from "cs" socket
+    Whenever a message is received, broadcast it to all other connected clietns
     """
     while True:
         try:
-            # keep listening for a message from `cs` socket
+            # keep listening for a message from 'cs' socket
             msg = cs.recv(1024).decode()
         except Exception as e:
-            # client no longer connected
-            # remove it from the set
+            # client no longer connection
+            # remove it from is the set
             print(f"[!] Error: {e}")
             client_sockets.remove(cs)
         else:
-            # if we received a message, replace the <SEP>
+            # if we received a message, replace the "SEP"
             # token with ": " for nice printing
             msg = msg.replace(separator_token, ": ")
         # iterate over all connected sockets
@@ -41,22 +42,23 @@ def listen_for_client(cs):
             # and send the message
             client_socket.send(msg.encode())
 
-
 while True:
-    # we keep listening for new connections all the time
+    # we keep listening for new connection all the time
     client_socket, client_address = s.accept()
     print(f"[+] {client_address} connected.")
-    # add the new connected client to connected sockets
+    # add the new connected  client to connected sockets
     client_sockets.add(client_socket)
+
     # start a new thread that listens for each client's messages
     t = Thread(target=listen_for_client, args=(client_socket,))
-    # make the thread daemon so it ends whenever the main thread ends
+    # make the thread daemon so it ends whenever the  main thread ends
     t.daemon = True
     # start the thread
     t.start()
 
 # close client sockets
 for cs in client_sockets:
-    cs.close()
+    cs.close
 # close server socket
-s.close()
+s.close
+
