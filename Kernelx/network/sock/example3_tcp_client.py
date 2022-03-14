353ce6a5a58fff71534db397bd512533901c9cc8
Sauytov Sadyr <sadyr0012@mail.com>
2022-03-14 22:21:49 +0600

    create chat

--- Kernelx/network/sock/example3_tcp_client.py
+++ Kernelx/network/sock/example3_tcp_client.py
@@ -1,27 +1,44 @@
 # example 1
 
 import socket
+
 # create tcp socket client
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# connecting in port 8888
-username = 'Sadyr'
-sock.connect(('', 8877))
-sock.send(username.encode())
 
+
+# connecting in port 8888
+def after_connection_to_server(server_socket):  # данная функция получает первое сообщение от сервера при подключении к серверу
+    welcome_msg_from_server = server_socket.recv(1024).decode()
+    if len(welcome_msg_from_server) > 0:
+        return print(f'Server: {welcome_msg_from_server}')
+
+
+def send_msg_to_server(server_socket, message):
+    sender = server_socket.send(message.encode())
+    print(f"You: {message}")
+
+
+def receive_msg_from_server(server_socket):
+    msg_from_server = server_socket.recv(1024).decode()
+    return f'Server: {msg_from_server}'
+
+
+username = socket.gethostname()
+sock.connect(('', 8888))
+sock.send(username.encode())
+after_connection_to_server(sock)
 
 while True:
-    msg_from_server = sock.recv(1024).decode()
-    if len(msg_from_server) == 0:
-        continue
+    msg = input()
+    if len(msg) > 0:
+        send_msg_to_server(sock, msg)
     else:
-        print(f'Сообщение от сервера: {msg_from_server}')
-    while True:
-        message_to_server = input()
-        msg_to_server = message_to_server.encode()
-        sock.send(msg_to_server)
         continue
 
-# close socket connection
-#sock.close()
+
+
+
+
+
 
 
--- Kernelx/network/sock/examplr3_tcp_server.py
+++ Kernelx/network/sock/examplr3_tcp_server.py
@@ -2,40 +2,48 @@
 import socket
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.bind(('127.0.0.1', 8877))
+sock.bind(('127.0.0.1', 8888))
 sock.listen(5)
 
 
-def recieve_msg(client_machine):  # Функция для приема сообщений от клиента
-    message = client_machine.recv(1024).decode()
-    return f'{username_client}:{message}'
+def receive_msg_from_client(client_address, client_username):  # Функция для приема сообщений от клиента
+    message = client_address.recv(1024).decode()
+    return f'{client_username}:{message}'
 
 
-def sender_msg(client_machine, text):  # Отправка сообщений клиенту
-    message = client_machine.send(text)
-    return f'Следующее сообщение отправлено клиенту: {text}'
+def sender_msg_to_client(client_address, client_username, text):  # Отправка сообщений клиенту
+    message = client_address.send(text.encode())
+    return f'Сообщение отправлено {client_username}: {text}'
+
+
+def welcome_msg_to_client(client_address):
+    client_username = client_address.recv(1024).decode()
+    welcome_msg = f' Добро пожаловать в чат {client_username} '.encode()
+    client_address.send(welcome_msg)
+    connect_message = f'{client_username} подключился'
+    return client_username, connect_message
 
 
 while True:
     try:
         print('Сервер ожидает соединения . . .')
         client, addr = sock.accept()
-
+        client_username, connect_msg = welcome_msg_to_client(client)
+        print(connect_msg)
     except KeyboardInterrupt:
         sock.close()
         break
     else:
-        username_client = client.recv(1024).decode()
-        text = f' Добро пожаловать в чат {username_client} '.encode()
-        sender_msg(client, text)
-        print(f'{username_client} подключился')
-    while True:
-        message_from_client = recieve_msg(client)
-        if len(message_from_client) > len(username_client)+1:
-            print(message_from_client)
+        while True:
+            msg_from_client = receive_msg_from_client(client, client_username)
+            if len(msg_from_client) > 0:
+                print(msg_from_client)
+            else:
+                continue
 
 
 
-        #client.close()
-        #print('Сокет закрыт')
-        #break
+
+
+
+
