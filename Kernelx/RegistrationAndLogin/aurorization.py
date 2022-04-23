8b5597def95adeebd68c2ad25bdbd0bd873f87bb
Sauytov Sadyr <sadyr0012@mail.com>
2022-04-23 22:25:17 +0600

    Create authorization and registration unit

--- Kernelx/RegistrationAndLogin/aurorization.py
+++ Kernelx/RegistrationAndLogin/aurorization.py
@@ -1,7 +1,27 @@
-username = 'svet'
-password = '1234l5'
+#username = 'svet'
+#password = '1234l5'
 user_db = 'user.txt'
 
+
+def user_registration():
+    print('Please provide me: ')
+    username = str(input('Your username: '))
+    password = str(input('Your password: '))
+    repeat_password = str(input('Repeat password: '))
+    if password != repeat_password:
+        while password != repeat_password:
+            print('Passwords not equivalent. Please repeate correctly password')
+            repeat_password = str(input('repeat password'))
+    u_p = username + ' ' + password + '\n'
+    db_users = open('user.txt', 'a')
+    db_users.write(u_p)
+    db_users.close()
+    print(f'Conglomerations {username}. Registrations success')
+    choice = str(input('You want aut or exit? if aut enter "a", if exit enter "q"'))
+    if choice.lower() == 'a':
+        start_aut()
+    if choice.lower() == 'q':
+        exit()
 
 def get_existing_users():
     with open(user_db,'r') as user_dba:
@@ -10,19 +30,6 @@ def get_existing_users():
             username, password = line.split()
             yield username, password
 
-
-# def is_authorized(username, password):
-# return any(user == (username, password) for user in get_existing_users())
-
-# def user_exists(username):
-#     return any((usr_name == username) for usr_name, _ in get_existing_users())
-# #
-# for i in get_existing_users():
-#     if username == i[0]:
-#         print(i[1])
-#         break
-#     else:
-#         continue
 def is_authorized(username, password):
     for user in get_existing_users():
         user_name, pass_word = user
@@ -33,13 +40,6 @@ def is_authorized(username, password):
             authorizide = False
             continue
     return authorizide
-
-
-if is_authorized(username, password):
-    print('user exists')
-else:
-    print('user not found')
-
 
 def user_exists(username):
     for user in get_existing_users():
@@ -52,15 +52,12 @@ def user_exists(username):
             continue
     return authorizide
 
-
-print(user_exists(username))
-
 def choice_menu():
     choice = str(input('Please choice point. If want authorized, enter "a", if want registration, enter "r". For exit enter "q"'))
     if choice.lower() == 'a':
         start_aut()
-    #if choice.lower() == 'r':
-        #user_registration()
+    if choice.lower() == 'r':
+        user_registration()
     if choice.lower() == 'q':
         exit()
 
@@ -72,10 +69,12 @@ def ask_user_credentials():
 
 def start_aut():
     username, password = ask_user_credentials()
-    if user_exists():
+    if user_exists(username):
         if is_authorized(username,password):
             print('Success aut')
         else:
             print('Incorrect password')
     else:
         print('user don"t found')
+
+choice_menu()
