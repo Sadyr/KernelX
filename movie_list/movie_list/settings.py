3e886029fe5354f0253f4cddb360aa5ccbea8b27
Sauytov Sadyr <sadyr0012@mail.com>
2022-05-25 14:28:40 +0600

    project subtitles5а

--- movie_list/movie_list/settings.py
+++ movie_list/movie_list/settings.py
@@ -31,13 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'list_app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'list_app'
+
 ]
 
 MIDDLEWARE = [
