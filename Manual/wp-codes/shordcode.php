3ff5011876c3de5ea5da2feae7a8175f0eb6ad50
Sauytov Sadyr <sadyr0012@mail.com>
2022-07-01 02:24:22 +0600

    Полное обновленгие

---
+++ Manual/wp-codes/shordcode.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Template Name: Таксономии-Газовый котел
+*/
+get_header();
+
+echo do_shortcode ('[wp_cart_button name="Test Product" price="29.95"]');
+echo do_shortcode ('[show_wp_shopping_cart]');
+
+
+?>
